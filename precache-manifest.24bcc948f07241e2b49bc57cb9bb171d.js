fff9e33d0d19bd2bf677460ae50c71e6278e7ad1
Deman250 <deman.250052@gmail.com>
2019-10-16 23:22:10 +0300

    Updates

---
+++ precache-manifest.24bcc948f07241e2b49bc57cb9bb171d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22238d4508e4e0b7f6fa2deb339f588a",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "dd6b4928a39f475b9b0d",
+    "url": "/react_todo-app/static/css/main.5da280ae.chunk.css"
+  },
+  {
+    "revision": "e613794f9e6ef196fb5a",
+    "url": "/react_todo-app/static/js/2.40a39426.chunk.js"
+  },
+  {
+    "revision": "dd6b4928a39f475b9b0d",
+    "url": "/react_todo-app/static/js/main.2ee37c79.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  },
+  {
+    "revision": "8b01731da55cd6dee988506de6f18066",
+    "url": "/react_todo-app/static/media/bg.8b01731d.jpg"
+  },
+  {
+    "revision": "bdfd90660e255931a9574cc6c4371fbd",
+    "url": "/react_todo-app/static/media/cloudy.bdfd9066.svg"
+  },
+  {
+    "revision": "6f7bcdff4245c15032ed65a6ecbf5a66",
+    "url": "/react_todo-app/static/media/light.6f7bcdff.svg"
+  },
+  {
+    "revision": "1e3e04ffdbe61c30795328f8df4b403e",
+    "url": "/react_todo-app/static/media/snow.1e3e04ff.svg"
+  },
+  {
+    "revision": "381339ecf9ca80876e4d0756ce80462d",
+    "url": "/react_todo-app/static/media/storm.381339ec.svg"
+  },
+  {
+    "revision": "58c341f068259cb955f5a68d69a35322",
+    "url": "/react_todo-app/static/media/sun.58c341f0.svg"
+  }
+]);
